683818d28693a3705c899ec7944ec409df897108
Julien Beasley <Julien.Beasley@amd.com>
2019-08-08 09:28:26 -0700

    added tuple

---
+++ RayTracer/Tuple.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayTracer
+{
+    public class Tuple
+    {
+        private static double epsilon = 0.000001;
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+        public double W { get; set; }
+
+        public Tuple(double x, double y, double z, double w)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+            this.W = w;
+        }
+
+        public bool isPoint()
+        {
+            if (W == 1)
+                return true;
+            return false;
+        }
+
+        public bool isVector()
+        {
+            if (W == 0)
+                return true;
+            return false;
+        }
+
+        public static Tuple point(double x, double y, double z)
+        {
+            return new Tuple(x, y, z, 1.0);
+        }
+
+        public static Tuple vector(double x, double y, double z)
+        {
+            return new Tuple(x, y, z, 0.0);
+        }
+
+        public static bool areEqual(Tuple a, Tuple b)
+        {
+            if (areClose(a.X, b.X) &&
+                areClose(a.Y, b.Y) &&
+                areClose(a.Z, b.Z) &&
+                areClose(a.W, b.W))
+                return true;
+            return false;
+        }
+
+        private static bool areClose(double a, double b)
+        {
+            if (Math.Abs(a-b) < epsilon)
+                return true;
+            return false;
+        }
+
+        public static Tuple operator +(Tuple a, Tuple b)
+        {
+            return new Tuple(a.X + b.X, a.Y + b.Y, a.Z + b.Z, a.W + b.W);
+        }
+
+        public static Tuple operator -(Tuple a, Tuple b)
+        {
+            return a + !b;
+        }
+
+        public static Tuple operator !(Tuple a)
+        {
+            return a * -1;
+        }
+
+        public static Tuple operator*(Tuple a, double b)
+        {
+            return new Tuple(a.X * b, a.Y * b, a.Z * b, a.W * b);
+        }
+
+        public static Tuple operator*(double b, Tuple a)
+        {
+            return a * b;
+        }
+
+    }
+
+}
---
+++ RayTracerTestProject/UnitTest.cs
@@ -0,0 +1,130 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RayTracer;
+
+namespace RayTracerTestProject
+{
+    [TestClass]
+    public class TupleUnitTest
+    {
+        [TestMethod]
+        public void TestTuplePoint()
+        {
+            RayTracer.Tuple tuple = new RayTracer.Tuple(4.3, -4.2, 3.1, 1.0);
+            Assert.AreEqual(4.3, tuple.X);
+            Assert.AreEqual(-4.2, tuple.Y);
+            Assert.AreEqual(3.1, tuple.Z);
+            Assert.AreEqual(1.0, tuple.W);
+            Assert.IsTrue(tuple.isPoint());
+            Assert.IsFalse(tuple.isVector());
+        }
+
+        [TestMethod]
+        public void TestTupleVector()
+        {
+            RayTracer.Tuple tuple = new RayTracer.Tuple(4.3, -4.2, 3.1, 0.0);
+            Assert.AreEqual(4.3, tuple.X);
+            Assert.AreEqual(-4.2, tuple.Y);
+            Assert.AreEqual(3.1, tuple.Z);
+            Assert.AreEqual(0.0, tuple.W);
+            Assert.IsFalse(tuple.isPoint());
+            Assert.IsTrue(tuple.isVector());
+        }
+
+        [TestMethod]
+        public void TestTuplePointFactory()
+        {
+            RayTracer.Tuple point = RayTracer.Tuple.point(4.3, -4.2, 3.1);
+            RayTracer.Tuple tuple = new RayTracer.Tuple(4.3, -4.2, 3.1, 1.0);
+            Assert.IsTrue(RayTracer.Tuple.areEqual(point, tuple));
+        }
+
+        [TestMethod]
+        public void TestTupleVectorFactory()
+        {
+            RayTracer.Tuple vector = RayTracer.Tuple.vector(4.3, -4.2, 3.1);
+            RayTracer.Tuple tuple = new RayTracer.Tuple(4.3, -4.2, 3.1, 0.0);
+            Assert.IsTrue(RayTracer.Tuple.areEqual(vector, tuple));
+        }
+
+        [TestMethod]
+        public void TestFloatEpsilon()
+        {
+            RayTracer.Tuple a = new RayTracer.Tuple(4.3, -4.2, 3.1, 0.0);
+            RayTracer.Tuple b = new RayTracer.Tuple(4.30000001, -4.19999999, 3.1, 0.0);
+            Assert.IsTrue(RayTracer.Tuple.areEqual(a, b));
+        }
+
+        [TestMethod]
+        public void TestAddition()
+        {
+            RayTracer.Tuple a = new RayTracer.Tuple(3, -2, 5, 1);
+            RayTracer.Tuple b = new RayTracer.Tuple(-2, 3, 1, 0);
+            RayTracer.Tuple c = new RayTracer.Tuple(1, 1, 6, 1);
+            Assert.IsTrue(RayTracer.Tuple.areEqual(a+b, c));
+        }
+
+        [TestMethod]
+        public void AddingTwoPointsDoesntGivePoint()
+        {
+            RayTracer.Tuple a = new RayTracer.Tuple(3, -2, 5, 1);
+            RayTracer.Tuple b = new RayTracer.Tuple(-2, 3, 1, 1);
+            RayTracer.Tuple c = a + b;
+            Assert.IsFalse(c.isPoint());
+            Assert.IsFalse(c.isVector());
+        }
+
+        [TestMethod]
+        public void SubtractPoints()
+        {
+            RayTracer.Tuple a = RayTracer.Tuple.point(3, 2, 1);
+            RayTracer.Tuple b = RayTracer.Tuple.point(5, 6, 7);
+            RayTracer.Tuple c = RayTracer.Tuple.vector(-2, -4, -6);
+            Assert.IsTrue(RayTracer.Tuple.areEqual(a - b, c));
+        }
+
+        [TestMethod]
+        public void SubtractVectorAndPoint()
+        {
+            RayTracer.Tuple a = RayTracer.Tuple.point(3, 2, 1);
+            RayTracer.Tuple b = RayTracer.Tuple.vector(5, 6, 7);
+            RayTracer.Tuple c = RayTracer.Tuple.point(-2, -4, -6);
+            Assert.IsTrue(RayTracer.Tuple.areEqual(a - b, c));
+        }
+
+        [TestMethod]
+        public void SubtractVectors()
+        {
+            RayTracer.Tuple a = RayTracer.Tuple.vector(3, 2, 1);
+            RayTracer.Tuple b = RayTracer.Tuple.vector(5, 6, 7);
+            RayTracer.Tuple c = RayTracer.Tuple.vector(-2, -4, -6);
+            Assert.IsTrue(RayTracer.Tuple.areEqual(a - b, c));
+        }
+
+        [TestMethod]
+        public void SubtractZeroVector()
+        {
+            RayTracer.Tuple zero = RayTracer.Tuple.vector(0, 0, 0);
+            RayTracer.Tuple v = RayTracer.Tuple.vector(1, -2, 3);
+            RayTracer.Tuple c = RayTracer.Tuple.vector(-1, 2, -3);
+            Assert.IsTrue(RayTracer.Tuple.areEqual(zero - v, c));
+        }
+
+        [TestMethod]
+        public void NegateTuple()
+        {
+            RayTracer.Tuple a = new RayTracer.Tuple(1, -2, 3, -4);
+            RayTracer.Tuple b = new RayTracer.Tuple(-1, 2, -3, 4);
+            Assert.IsTrue(RayTracer.Tuple.areEqual(!a, b));
+        }
+
+        [TestMethod]
+        public void ScalarMultiplicationTuple()
+        {
+            RayTracer.Tuple a = new RayTracer.Tuple(1, -2, 3, -4);
+            RayTracer.Tuple b = new RayTracer.Tuple(3.5, -7, 10.5, -14);
+            Assert.IsTrue(RayTracer.Tuple.areEqual(3.5 * a, b));
+        }
+    }
+}
